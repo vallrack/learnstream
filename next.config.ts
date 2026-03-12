c7557cb1ac5dfb4ee78571975a5f0d0103589259
Danny Morales <programming2025dan@gmail.com>
2026-03-12 18:19:32 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://dprogra

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'dprogramadores.com.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
